cab5d243381e8b4e50c9a2fc5cb0711b4b49c33e
firebushtree <firebushtree@163.com>
2025-09-01 11:27:16 +0800

    feat: add contract buy and sell

--- client/src/config/tokens.ts
+++ client/src/config/tokens.ts
@@ -1,7 +1,7 @@
 import { localhost, mainnet, sepolia } from 'wagmi/chains'
 
 export const OWC_TOKEN_ADDRESSES = {
-  [localhost.id]: '0xf38307b674C20f11f212B888f581d76CF0d485D3',
+  [localhost.id]: '0x0aC59E0181418D1b73058729b6d187697f73AEd8',
   [sepolia.id]: '0x1234567890123456789012345678901234567890',
   [mainnet.id]: '0x1234567890123456789012345678901234567890',
 } as const
--- contract/contracts/OWCToken.sol
+++ contract/contracts/OWCToken.sol
@@ -5,8 +5,29 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract OWCToken is ERC20, Ownable {
+    uint256 public rate;
+
     constructor() ERC20("OWC", "OW") Ownable(msg.sender) {
+        rate = 5000;
         _mint(msg.sender, 1000000 * 10**decimals());
+    }
+
+    function buyTokens() external payable {
+        require(msg.value > 0, "Send ETH to buy tokens");
+        uint256 tokenAmount = msg.value * rate;
+        require(balanceOf(address(this)) >= tokenAmount, "Not enough tokens");
+        _transfer(address(this), msg.sender, tokenAmount);
+    }
+
+    function sellTokens(uint256 tokenAmount) external {
+        require(tokenAmount > 0, "Specify token amount");
+        require(balanceOf(msg.sender) >= tokenAmount, "Not enough tokens");
+
+        uint256 ethAmount = tokenAmount / rate;
+        require(address(this).balance >= ethAmount, "Not enough ETH");
+
+        _transfer(msg.sender, address(this), tokenAmount);
+        payable(msg.sender).transfer(ethAmount);
     }
 
     function mint(address to, uint256 amount) public onlyOwner {
