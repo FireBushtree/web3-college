fb962c0ac82d73134aaf42fa00947ed7b1f9372f
firebushtree <firebushtree@163.com>
2025-09-02 15:09:15 +0800

    feat: remove background icon

--- client/src/components/Layout.tsx
+++ client/src/components/Layout.tsx
@@ -1,5 +1,4 @@
 import type { ReactNode } from 'react'
-import BackgroundIcons from './BackgroundIcons'
 import Header from './Header'
 
 interface LayoutProps {
@@ -9,7 +8,6 @@ interface LayoutProps {
 export default function Layout({ children }: LayoutProps) {
   return (
     <div className="min-h-screen bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 relative">
-      <BackgroundIcons />
       <Header />
       <main className="container mx-auto px-4 pt-24 pb-8 relative z-10">
         {children}
