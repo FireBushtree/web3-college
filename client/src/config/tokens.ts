4789ad6f75d214fed6a3bbd7871d737683d39866
firebushtree <firebushtree@163.com>
2025-09-03 09:45:29 +0800

    feat: update token config

--- client/src/config/tokens.ts
+++ client/src/config/tokens.ts
@@ -4,14 +4,14 @@ import CourseRegistryABI from '@/assets/CourseRegistry.json'
 import OWCTokenABI from '@/assets/OWCToken.json'
 
 export const OWC_TOKEN_ADDRESSES = {
-  [localhost.id]: OWCTokenABI.networks[5777].address as Address,
-  [sepolia.id]: '0x1234567890123456789012345678901234567890',
+  // [localhost.id]: OWCTokenABI.networks[5777].address as Address,
+  [sepolia.id]: '0xF74aE851eeFb542d77581E68FE3D0dFF2C259ddC',
   [mainnet.id]: '0x1234567890123456789012345678901234567890',
 } as const
 
 export const COURSE_REGISTRY_ADDRESSES = {
-  [localhost.id]: CourseRegistryABI.networks[5777].address as Address,
-  [sepolia.id]: '0x0000000000000000000000000000000000000000',
+  // [localhost.id]: CourseRegistryABI.networks[5777].address as Address,
+  [sepolia.id]: '0xeA8A709014325a6cffD9AcDA46856A54113D7cA3',
   [mainnet.id]: '0x0000000000000000000000000000000000000000',
 } as const
 
